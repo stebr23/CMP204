c1d30599e352d39d653b8c96b60e0a1e1fd5cf9c
stephen.r.brayson <stephen.r.brayson@accenture.com>
2018-02-12 10:25:10 +0000

    Started on Views

--- TuringAda/TuringAda/App_Start/RouteConfig.cs
+++ TuringAda/TuringAda/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace TuringAda
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
         }
     }
 }
---
+++ TuringAda/TuringAda/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TuringAda.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method, " + Hello("Emily") + ", " ;
+        //}
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+
+        public string Hello(string name)
+        {
+            return "Hello, " + name + "! " + Welcome("Emily");
+        }
+
+    }
+}
